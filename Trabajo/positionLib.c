705dd3e9b82422be6df66df945f6f36c98e6a84d
Jaime <jai_mi_to@hotmail.es>
2016-05-28 13:57:13 +0200

    Documented positionLib.c

--- Trabajo/positionLib.c
+++ Trabajo/positionLib.c
@@ -1,15 +1,23 @@
+/*
+ *  positionLib.c
+ *  Contains methods to modify the robot position easily and angle normalization
+ */
+
 // GLOBAL VARIABLE FOR ODOMETRY
 #define INI_X 0
 #define INI_Y 0
 #define INI_TH 0
 
+// Custom struct
 typedef struct {
      float x;   //x coordinate
      float y;   //y coordinate
      float th;  //theta - orientation RADIANES
 }TPosition;
 
-
+/*
+ *  Modifies the robot position
+ */
 void set_position(TPosition &pos, float x, float y, float th)
 {
   pos.x=x;
@@ -17,6 +25,9 @@ void set_position(TPosition &pos, float x, float y, float th)
   pos.th=th;
 }
 
+/*
+ *  Normalizes the given angle
+ */
 void normalizeAngle(float & angle){
 // normalize angle in radians between PI and -PI
 
